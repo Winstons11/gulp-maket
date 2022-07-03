e5a1d368871905fc9aab1ec0c887e6d15a081fee
Winstons11 <75261174+Winstons11@users.noreply.github.com>
2022-07-03 12:35:24 +0300

    Add files via upload

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,2 +1,6 @@
 import GraphTabs from 'graph-tabs';
-const tabs = new GraphTabs('popup');
+const tabs1 = new GraphTabs('popup1');
+const tabs2 = new GraphTabs('popup2');
+const tabs3 = new GraphTabs('popup3');
+const tabs4 = new GraphTabs('popup4');
+const tabs5 = new GraphTabs('popup5');
